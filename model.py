d30da1e0d68b1c8fbd77bcfdc1c5624d4d81287f
Michael Stanley <michaelstanley@admins-MacBook-Pro.local>
2020-05-12 10:35:41 -0400

    added affine=False to the batchnorm2d layers

--- model.py
+++ model.py
@@ -13,40 +13,40 @@ class _netG(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
             # state size: (nef) x 64 x 64
             nn.Conv2d(opt.nef,opt.nef,4,2,1, bias=False),
-            nn.BatchNorm2d(opt.nef),
+            nn.BatchNorm2d(opt.nef, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size: (nef) x 32 x 32
             nn.Conv2d(opt.nef,opt.nef*2,4,2,1, bias=False),
-            nn.BatchNorm2d(opt.nef*2),
+            nn.BatchNorm2d(opt.nef*2, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size: (nef*2) x 16 x 16
             nn.Conv2d(opt.nef*2,opt.nef*4,4,2,1, bias=False),
-            nn.BatchNorm2d(opt.nef*4),
+            nn.BatchNorm2d(opt.nef*4, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size: (nef*4) x 8 x 8
             nn.Conv2d(opt.nef*4,opt.nef*8,4,2,1, bias=False),
-            nn.BatchNorm2d(opt.nef*8),
+            nn.BatchNorm2d(opt.nef*8, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size: (nef*8) x 4 x 4
             nn.Conv2d(opt.nef*8,opt.nBottleneck,4, bias=False),
             # tate size: (nBottleneck) x 1 x 1
-            nn.BatchNorm2d(opt.nBottleneck),
+            nn.BatchNorm2d(opt.nBottleneck, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # input is Bottleneck, going into a convolution
             nn.ConvTranspose2d(opt.nBottleneck, opt.ngf * 8, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(opt.ngf * 8),
+            nn.BatchNorm2d(opt.ngf * 8, affine=False),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(opt.ngf * 8, opt.ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ngf * 4),
+            nn.BatchNorm2d(opt.ngf * 4, affine=False),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d(opt.ngf * 4, opt.ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ngf * 2),
+            nn.BatchNorm2d(opt.ngf * 2, affine=False),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(opt.ngf * 2, opt.ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ngf),
+            nn.BatchNorm2d(opt.ngf, affine=False),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d(opt.ngf, opt.nc, 4, 2, 1, bias=False),
@@ -72,15 +72,15 @@ class _netlocalD(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(opt.ndf, opt.ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ndf * 2),
+            nn.BatchNorm2d(opt.ndf * 2, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*2) x 16 x 16
             nn.Conv2d(opt.ndf * 2, opt.ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ndf * 4),
+            nn.BatchNorm2d(opt.ndf * 4, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*4) x 8 x 8
             nn.Conv2d(opt.ndf * 4, opt.ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(opt.ndf * 8),
+            nn.BatchNorm2d(opt.ndf * 8, affine=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf*8) x 4 x 4
             nn.Conv2d(opt.ndf * 8, 1, 4, 1, 0, bias=False),
